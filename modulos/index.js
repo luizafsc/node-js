8ca024c7e041e7420bec50e9fa2fc70702292db8
luiza fsc <luizascrs@gmail.com>
2023-04-11 11:07:48 -0300

    aula sobre modulos

---
+++ modulos/index.js
@@ -0,0 +1,6 @@
+const calculator = require('./calculator');
+
+console.log(calculator.add(2, 3)); // 5
+console.log(calculator.subtract(5, 3)); // 2
+console.log(calculator.multiply(2, 3)); // 6
+console.log(calculator.divide(6, 3)); // 2
---
+++ modulos/meuModulo.js
@@ -0,0 +1,9 @@
+function minhaFuncao() {
+    console.log("Olá do meu módulo!");
+    }
+    
+    module.exports = {
+    minhaFuncao: minhaFuncao
+    };
+    
+    
